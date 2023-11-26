53f7812a56cff1518e53eaf5772109cd4c003658
Kago <kmotlhabane769@gmail.com>
2023-11-26 13:29:55 +0200

    Clear inputs on sign-up if all fields valid

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ let signUpBtn = document.querySelector('.sign_up');
 let pwd = document.getElementById('pwd');
 let pwdConfirm = document.getElementById('confirm_pwd');
 let form = document.querySelector('.form');
-let firstName = document.getElementById('first_name');
 let lastName = document.getElementById('last_name');
+let firstName = document.getElementById('first_name');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
 
 let error = document.createElement('section');
 error.classList.add('error');
@@ -12,7 +14,7 @@ error.textContent = "";
 
 signUpBtn.addEventListener('click', () => {
     let errorsPresent = false;
-    if (form.querySelector('.error') !== null){
+    if (form.querySelector('.error') !== null) {
         error.textContent = "";
         form.removeChild(error);
     }
@@ -30,22 +32,27 @@ signUpBtn.addEventListener('click', () => {
     if (pwd.value !== pwdConfirm.value) {
         errorsPresent = true;
         error.textContent += "⚠ Passwords do not match.";
-    } 
+    }
 
     if (pwd.value.length < 8) {
         errorsPresent = true;
         error.textContent += "⚠ Each password must be length 8.";
-    } 
+    }
 
     if (errorsPresent) {
         form.appendChild(error);
         return;
     }
-    
+
     if (!errorsPresent) {
-        if (form.querySelector('.error') !== null){
-            error.textContent = "";
+        if (form.querySelector('.error') !== null) {
+            error.textContent = ""; 
             form.removeChild(error);
         }
+        firstName.value = ""; 
+        lastName.value = ""; 
+        email.value = ""; 
+        pwd.value = ""; 
+        pwdConfirm.value = "";
     }
 });
